f08c15e01687b48d20886b8078c241a8506f8c33
Yurii_Ivanov <128720591+IvanovYurii@users.noreply.github.com>
2023-11-01 13:31:47 +0200

    Spring Boot web

---
+++ src/main/java/ivanov/springbootintro/controller/BookStoreController.java
@@ -0,0 +1,35 @@
+package ivanov.springbootintro.controller;
+
+import ivanov.springbootintro.dto.BookDto;
+import ivanov.springbootintro.dto.CreateBookRequestDto;
+import ivanov.springbootintro.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/books")
+public class BookStoreController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BookDto findById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    public BookDto save(@RequestBody CreateBookRequestDto requestDto) {
+        return bookService.save(requestDto);
+    }
+}

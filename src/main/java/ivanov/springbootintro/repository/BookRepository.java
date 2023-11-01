a1b5968bc5c638aa0fa2deee616197764ead5e5f
Yurii_Ivanov <128720591+IvanovYurii@users.noreply.github.com>
2023-11-01 16:55:50 +0200

    Spring Boot Data JPA

--- src/main/java/ivanov/springbootintro/repository/BookRepository.java
+++ src/main/java/ivanov/springbootintro/repository/BookRepository.java
@@ -1,13 +1,9 @@
 package ivanov.springbootintro.repository;
 
 import ivanov.springbootintro.model.Book;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
+public interface BookRepository extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
 }

63670c9fad37e5671b5a4af28c26b697afc92075
Yurii_Ivanov <128720591+IvanovYurii@users.noreply.github.com>
2023-11-05 00:14:17 +0200

    Pagination, Sorting, Swagger (http://localhost:8080/swagger-ui/index.html#/)

--- src/main/java/ivanov/springbootintro/dto/CreateBookRequestDto.java
+++ src/main/java/ivanov/springbootintro/dto/CreateBookRequestDto.java
@@ -1,17 +1,18 @@
 package ivanov.springbootintro.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotEmpty
     private String title;
-    @NotNull
+    @NotEmpty
     private String author;
-    @NotNull
+    @NotEmpty
     private String isbn;
     @NotNull
     @Min(0)
